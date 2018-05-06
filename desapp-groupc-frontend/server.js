79bfff585292344986f5eb78b56fe5e4d0b3982b
Noel <witherwings77@gmail.com>
2018-05-06 15:05:48 -0300

    test 1231231231231231212312312312321

--- desapp-groupc-frontend/server.js
+++ desapp-groupc-frontend/server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/'))
+
+app.get('*', (req, res) =>{
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
