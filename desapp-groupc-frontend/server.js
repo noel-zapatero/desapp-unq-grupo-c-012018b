2eea342340e3fe10da0c613ed92c8db3febf56a3
Noel <witherwings77@gmail.com>
2018-05-06 15:33:44 -0300

    ´ñ

--- desapp-groupc-frontend/server.js
+++ desapp-groupc-frontend/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname)
+app.use(express.static(__dirname + '/'))
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
