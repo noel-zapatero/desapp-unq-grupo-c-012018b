91883e47af0fbeda6dbd76b1cc74ef1336d1fd68
Noel <witherwings77@gmail.com>
2018-05-06 15:24:18 -0300

    ftftgt

--- desapp-groupc-frontend/server.js
+++ desapp-groupc-frontend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const path = require("path");
+const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/'))
+app.use(express.static(__dirname + '../app'))
 
-app.get('*', (req, res) =>{
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.get('/', function(req, res) {
+  res.sendfile(path.join(__dirname, '../app/index.html'));
 });
 
 // Start the app by listening on the default
