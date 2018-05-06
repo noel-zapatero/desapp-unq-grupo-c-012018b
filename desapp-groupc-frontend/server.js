59670a582182d9245c9f980b4a4798f197c68c1d
Noel <witherwings77@gmail.com>
2018-05-06 14:01:34 -0300

    test

--- desapp-groupc-frontend/server.js
+++ desapp-groupc-frontend/server.js
@@ -1,6 +1,7 @@
-//Install express server
+/*//Install express server
 const express = require('express');
 const path = require('path');
+const PORT = process.env.PORT || 5000
 
 const app = express();
 
@@ -14,3 +15,15 @@ app.get('/*', function(req,res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+*/
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
