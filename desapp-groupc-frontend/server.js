e214b58441f6fc9bbc0e21ff1e67a2d0dafdb66c
Noel <witherwings77@gmail.com>
2018-05-06 15:46:38 -0300

    adfqrw

--- desapp-groupc-frontend/server.js
+++ desapp-groupc-frontend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/'))
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.resolve('src', 'app', 'index.html') );
 });
 
 // Start the app by listening on the default
