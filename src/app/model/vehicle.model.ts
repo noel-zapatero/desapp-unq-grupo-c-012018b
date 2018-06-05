6747cf0cfd75e3d2ae7b818c164ec1e6a04c1526
Luky <lucas.wfh@gmail.com>
2018-06-05 16:28:03 -0400

    vehicle model

--- src/app/model/vehicle.model.ts
+++ src/app/model/vehicle.model.ts
@@ -6,6 +6,13 @@ export class Vehicle {
     zone: string;
     withdrawAddress: string;
     returnAddresses: [string];
-    
+    description: string;
+    contactPhone: string;
+    rentFeeDay: number;
+    rentFeeHour: number;
+
+    ownerId: number;
+    ownerName: string;
+    ownerLastName: string;
     
 }
