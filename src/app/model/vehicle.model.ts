b8a99d87e25c6876bd625c0221875d29e1b5e0bf
Luky <lucas.wfh@gmail.com>
2018-06-05 16:27:09 -0400

    vehicle model

--- src/app/model/vehicle.model.ts
+++ src/app/model/vehicle.model.ts
@@ -1,3 +1,11 @@
 export class Vehicle {
+
+    vehicleId: number;
+    type: string;
+    passengerCapacity: number;
+    zone: string;
+    withdrawAddress: string;
+    returnAddresses: [string];
+    
     
 }
