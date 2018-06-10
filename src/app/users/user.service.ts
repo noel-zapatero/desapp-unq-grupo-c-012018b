5bdea938408df76498e9d21604df2629522aa05a
wolfx <wolfx@wolfxs-MacBook-Pro.local>
2018-06-10 16:36:20 -0400

    charge and withdraw credits service

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -12,10 +12,39 @@ export class UserService {
 
     URL = 'http://localhost:8080/main/webservices/users';
 
+    options = {
+        headers: new HttpHeaders({
+            'Content-Type':  'application/json',
+            // TODO: agregar authorizartion token 
+        })
+    }
+
     constructor(private http: HttpClient) { }
 
     userLogIn(user,auth:AuthService): Observable<User> {
         return this.http.post<User>(this.URL, user, httpOptions(auth));
+    }
+
+    chargeCredits(userEmail:String, credits:number): Observable<User> {
+        return this.http.post<User>(
+            this.URL + '/chargecredits',
+            {
+                'userEmail': userEmail,
+                'credits': credits
+            },
+            this.options
+        );
+    }
+
+    withdrawCredits(userEmail:String, credits:number): Observable<User> {
+        return this.http.post<User>(
+            this.URL + '/withdrawcredits',
+            {
+                'userEmail': userEmail,
+                'credits': credits
+            },
+            this.options
+        );
     }
 
 }
